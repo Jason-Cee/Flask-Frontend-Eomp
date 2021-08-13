3b453d7f2b429dae413b6fceb96537683a136d50
Jason-Cee <jasoncee017@gmail.com>
2021-08-13 16:24:42 +0200

    Day Four

--- scripts/app.js
+++ scripts/app.js
@@ -35,8 +35,9 @@ let products = [
 
 for (let j = 0; j < carts.length; j++) {
   carts[j].addEventListener("click", () => {
+    cartNumbers(products[j]);
+    totalCost(products[j]);
     // console.log("Added to Cart");
-    cartNumbers();
   });
 }
 
@@ -48,7 +49,7 @@ function trueCartNumber() {
   }
 }
 
-function cartNumbers() {
+function cartNumbers(products) {
   let productNumbers = localStorage.getItem("cartNumbers");
 
   productNumbers = parseInt(productNumbers);
@@ -60,5 +61,66 @@ function cartNumbers() {
     localStorage.setItem("cartNumbers", 1);
     document.querySelector(".cart span").textContent = 1;
   }
+
+  setItems(products);
 }
+
+function setItems(products) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[products.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [products.tag]: products,
+      };
+    }
+    cartItems[products.tag].inCart += 1;
+  } else {
+    products.inCart = 1;
+    cartItems = {
+      [products.tag]: products,
+    };
+  }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(products) {
+  // console.log("the products price is", products.price);
+  let cartCost = localStorage.getItem("totalCost");
+
+  console.log("My cart cost is: ", cartCost);
+
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCost", cartCost + products.price);
+  } else {
+    localStorage.setItem("totalCost", products.price);
+  }
+}
+
+function displayCart() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  let productContainer = document.querySelector(".products-container");
+
+  console.log(cartItems);
+  if (cartItems && productContainer) {
+    productContainer.innerHTML = "";
+    Object.values(cartItems).map((item) => {
+      productContainer.innerHTML += `
+      <div class="product">
+        <i class="fas fa-window-close"></i>
+        <img src="./images/${item.tag}.jpeg">
+        <span>${item.name}</span>
+      </div>
+      `;
+    });
+  }
+}
+
 trueCartNumber();
+displayCart();
